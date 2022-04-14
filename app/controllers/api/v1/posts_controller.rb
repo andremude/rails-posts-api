8059844d4d96c9924b8fb35a87c357bcd3d7e464
Andres Munoz <andremude@hotmail.com>
2022-04-13 22:18:16 -0500

    posts api

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  # GET
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET
+  def show
+    render json: @post
+  end
+
+  # POST
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: api_v1_post_path(@post)
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE
+  def destroy
+    @post.destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
