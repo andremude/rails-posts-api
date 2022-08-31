c24ba494d5b02713825491b3e93bd80ff7a36bf3
Andres Munoz <andremude@hotmail.com>
2022-08-31 12:18:33 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
 
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 5 }
+
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
 end
