6332faaf694f10aeeae2446cf45b2a3266da3bf1
Andres Munoz <andremude@hotmail.com>
2022-09-01 21:24:42 -0500

    update

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,7 +2,6 @@ class PostSerializer < ActiveModel::Serializer
    attributes :id, :title, :content, :user
 
    def username
-      #You try byebug and check object
       object.username
    end
 end
